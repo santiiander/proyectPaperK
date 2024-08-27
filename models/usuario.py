65904c0a24292e9f6476516acaf9981b56987609
Santiago Andermatten <santiagoandermatten1@gmail.com>
2024-08-27 14:20:20 -0300

    quitamos railway pusimos mysql local

--- models/usuario.py
+++ models/usuario.py
@@ -4,7 +4,7 @@ from config.database import Base
 class Usuario(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)  # Asegúrate de que el nombre aquí coincida con el nombre de la columna en la base de datos
-    nombre = Column(String)
-    descripcion = Column(String)
+    email = Column(String(255), unique=True, index=True)  # Especifica la longitud de la cadena
+    hashed_password = Column(String(255))  # Especifica la longitud de la cadena
+    nombre = Column(String(255))  # Especifica la longitud de la cadena
+    descripcion = Column(String(255))  # Especifica la longitud de la cadena
