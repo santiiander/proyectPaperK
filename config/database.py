c6d74823e74749b6292d720ee7bb0ebe1ed313ba
Santiago Andermatten <santiagoandermatten1@gmail.com>
2024-08-29 16:24:05 -0300

    cambio db

--- config/database.py
+++ config/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from typing import Generator
 from sqlalchemy.orm import sessionmaker, declarative_base
-DATABASE_URL = "mysql+mysqlconnector://root:admin@localhost:3306/paperk"
-#DATABASE_URL = "mysql://root:EoKjoMdSXvhXRVqUtNOXxsrIIohDiqQT@autorack.proxy.rlwy.net:15617/railway"
+#DATABASE_URL = "mysql+mysqlconnector://root:admin@localhost:3306/paperk"
+DATABASE_URL = "mysql://root:EoKjoMdSXvhXRVqUtNOXxsrIIohDiqQT@autorack.proxy.rlwy.net:15617/railway"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
