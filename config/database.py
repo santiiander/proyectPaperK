c4792d24e3691e446525f72af8f38f6ba45c5a9d
Santiago Andermatten <santiagoandermatten1@gmail.com>
2024-09-06 13:41:36 -0300

    test

--- config/database.py
+++ config/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Cambia a mysql+mysqlconnector para usar el controlador correcto
 DATABASE_URL = "mysql+mysqlconnector://root:EoKjoMdSXvhXRVqUtNOXxsrIIohDiqQT@autorack.proxy.rlwy.net:15617/railway"
-#Comentario
+#Comentarionuevoxd
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
