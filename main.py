d0872e7e1ac89586f3d62ec460f2d06effd0d355
Santiago Andermatten <santiagoandermatten1@gmail.com>
2024-09-12 14:18:13 -0300

    fix back

--- main.py
+++ main.py
@@ -57,3 +57,20 @@ app.include_router(proyecto.router, prefix="/proyectos", tags=["Proyectos"])
 @app.get("/")
 def read_root():
     return {"message": "Bienvenido a la API de proyectos de origami"}
+
+
+@app.get("/firebase-config")
+async def get_firebase_config():
+    return {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),  # Asegúrate de reemplazar \n por saltos de línea
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
+}
